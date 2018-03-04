80df95087b028921e4b70e925a265fc6f71bb09a
Angela <awambuikanyi@gmail.com>
2018-03-04 17:52:04 +0300

    folder structure

---
+++ js/scripts.js
@@ -0,0 +1,24 @@
+var i, value;
+ $(document).ready(function() {
+   $("#ping-pong").submit(function(event) {
+     event.preventDefault();
+     value = parseInt($("#number").val());
+     var result = answer(value);
+
+     $("#push").text(result);
+   });
+ });
+
+ var answer=function() {
+ for (i = 1; i <= value; i++){
+   if (i % 15 === 0) {
+      $("#push").append("<li>"+"pingpong"+"</li>");
+   }else if (i % 3 === 0) {
+       $("#push").append("<li>"+"ping"+"</li>");
+   }else if (i % 5 === 0) {
+       $("#push").append("<li>"+"pong"+"</li>");
+   }else {
+      $("#push").append("<li>"+i+"</li>");
+   }
+ };
+};
